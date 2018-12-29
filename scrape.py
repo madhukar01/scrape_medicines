a49c95e4772ed7aacdd0d53966b664e984347cb4
Madhukar S Holla <mholla8@gmail.com>
2018-12-29 17:13:20 +0530

    Script v0.3

--- scrape.py
+++ scrape.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup as bs
 import xlsxwriter
 import pickle
 
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabets = ['l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 max_pages = 1000
 url = 'https://www.medindia.net/drug-price/brand-index.asp?alpha={alphabet}&page={page_number}'
 medicines_list = []
@@ -39,17 +40,20 @@ for alpha in alphabets:
 
                         medicine_page = urlopen(medicine_url)
                         medicine_html = bs(medicine_page, 'html.parser')
-                        div = medicine_html.find('div', {'class':'bbox'})
-                        spans = div.findAll('span')
-                        generic_name = spans[2].getText().split(':')[1].strip()
-                        dose = spans[3].getText().strip()
-
-                        medicines_list.append((medicine_name, generic_name, dose))
-                        worksheet.write(count, 0, medicine_name)
-                        worksheet.write(count, 1, generic_name)
-                        worksheet.write(count, 2, dose)
-                        count += 1
-                        print(generic_name, dose, '\n')
+                        try:
+                            div = medicine_html.find('div', {'class':'bbox'})
+                            spans = div.findAll('span')
+                            generic_name = spans[2].getText().split(':')[1].strip()
+                            dose = spans[3].getText().strip()
+                        except:
+                            continue
+                        else:
+                            medicines_list.append((medicine_name, generic_name, dose))
+                            worksheet.write(count, 0, medicine_name)
+                            worksheet.write(count, 1, generic_name)
+                            worksheet.write(count, 2, dose)
+                            count += 1
+                            print(generic_name, dose, '\n')
         print('Total medicines obtained: ', count)
         print('Total pages crawled: ', pages_crawled)
     workbook.close()
